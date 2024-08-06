393aefc07f0aa706fc930905a45ee6fce53bc363
XS <134421185+khushbakhtx@users.noreply.github.com>
2024-08-06 10:48:18 +0500

    Update 4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -6,8 +6,7 @@ select
     round(sum(s.unit_price * s.quantity) / (select sum(unit_price * quantity) from sales) * 100, 2) as market_share_percentage
 from 
     sales s
-join 
-    product p on s.product_id = p.product_id
+join product p on s.product_id = p.product_id
 group by 
     p.product_id, p.product_name;
 

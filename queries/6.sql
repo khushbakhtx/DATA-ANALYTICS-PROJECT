4f4022793938d07d3ad2f035f4a5b68a7792973b
XS <134421185+khushbakhtx@users.noreply.github.com>
2024-08-06 10:49:06 +0500

    Update 6.sql

--- queries/6.sql
+++ queries/6.sql
@@ -9,10 +9,8 @@ select
     rank() over (partition by st.store_id order by sum(s.quantity) desc) as product_rank
 from 
     sales s
-join 
-    store st on s.product_id = st.store_id
-join 
-    product p on s.product_id = p.product_id
+join store st on s.product_id = st.store_id
+join product p on s.product_id = p.product_id
 group by 
     s.sales_date, st.store_id, st.store_name, p.product_id, p.product_name
 order by 
